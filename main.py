ea540a9fe1b5c7a5bb3a0b575b4943f97e65a323
in-wei <83713727+in-wei@users.noreply.github.com>
2026-03-06 15:26:24 +0800

    Update main.py

--- main.py
+++ main.py
@@ -155,7 +155,7 @@ def handle_message(event: MessageEvent):
                     else:
                         analysis = analyze_stock_trend(stock_code, period)
                         reply_text = f"{CONFIG['response_prefix']}：\n{analysis}"
-            elif text.startswitch("/幫助"):
+            elif text.startswitch("/幫助"): 
                 reply_text = "/分析 [股票代碼] [1d|5d|1mo|3mo|6mo|1y|2y|5y|10y|ytd|max]\nex:/分析 2330 1y"
             else:
                 reply_text = f"{CONFIG['response_prefix']}：你想對 {text} 做什麼呢? Ex: 分析 2330 1y"
