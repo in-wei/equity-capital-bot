2c791189d7a10e3c47046f290c03737f6a55d480
in-wei <83713727+in-wei@users.noreply.github.com>
2026-03-08 11:16:42 +0800

    Update main.py

--- main.py
+++ main.py
@@ -180,7 +180,7 @@ def handle_message(event: MessageEvent):
                             print(f"目前嘗試{test_code}")
                             try:
                                 stock = yf.Ticker(test_code)
-                                hist = stock.history(period="5d)  # 先用短期間測試是否有效
+                                hist = stock.history(period="5d")  # 先用短期間測試是否有效
                                 if not hist.empty:
                                     stock_code = test_code
                                     used_suffix = suffix if suffix else "美股/無後綴"
